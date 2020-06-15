2729c92439f6da0f8cf945e6771bef6e4f9ce70f
Obehi <obehi.iredia@gmail.com>
2020-06-14 21:59:21 -0400

    Comment code

--- main.go
+++ main.go
@@ -13,18 +13,25 @@ func main() {
 	var md5hash string
 	fmt.Scanln(&md5hash)
 
+	/* Opening word list I found online
+	   this word list will each password will
+	   be hashed and checked against the provided hash
+	   if match found password found.*/
 	wl, err := os.Open("passwordlist.txt")
 	if err != nil {
 		fmt.Println("Error when opening passwordlist.txt")
 	}
-	defer wl.Close()
+	defer wl.Close() // Clean up, close file to save resources.
 
 	scanner := bufio.NewScanner(wl)
 	for scanner.Scan() {
 		password := scanner.Text()
+		// Hashing password read from password file.
 		hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
 		if hash == md5hash {
 			fmt.Printf("Password found!\n md5:\n%s", password)
+		} else {
+			fmt.Println("Password not found...")
 		}
 	}
 }
